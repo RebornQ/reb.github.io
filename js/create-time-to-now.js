526c4c09f160bed6173fac2b56bdf5471a9b51e6
Reborn <623844292@qq.com>
2018-08-21 22:08:47 +0800

    update

---
+++ js/create-time-to-now.js
@@ -0,0 +1,30 @@
+var now = new Date();
+
+function createtime() {
+    var grt = new Date("07/16/2018 15:00:00");//此处修改你的建站时间或者网站上线时间
+    now.setTime(now.getTime() + 250);
+    years = (now - grt) / 1000 / 60 / 60 / 24 / 365;
+    ynum = Math.floor(years);
+    days = (now - grt) / 1000 / 60 / 60 / 24;
+    dnum = Math.floor(days);
+    hours = (now - grt) / 1000 / 60 / 60 - (24 * dnum);
+    hnum = Math.floor(hours);
+    if (String(hnum).length == 1) {
+        hnum = "0" + hnum;
+    }
+    minutes = (now - grt) / 1000 / 60 - (24 * 60 * dnum) - (60 * hnum);
+    mnum = Math.floor(minutes);
+    if (String(mnum).length == 1) {
+        mnum = "0" + mnum;
+    }
+    seconds = (now - grt) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum);
+    snum = Math.round(seconds);
+    if (String(snum).length == 1) {
+        snum = "0" + snum;
+    }
+    document.getElementById("timeDate").innerHTML = /*"本站已运行 " +ynum+" 年 "*/ +dnum + " 天 ";
+    /*因为建站时间还没有一年，就将之注释掉了。需要的可以取消*/
+    document.getElementById("times").innerHTML = hnum + " 小时 " + mnum + " 分 " + snum + " 秒";
+}
+
+setInterval("createtime()", 250);
---
+++ js/my.js
@@ -0,0 +1,24 @@
+function SetCwinHeight(id) {
+    var iframeid = document.getElementById(id); //iframe id
+    if (document.getElementById) {
+        if (iframeid && !window.opera) {
+    		console.log("iframeid:" + iframeid.contentDocument);
+            if (iframeid.contentDocument && iframeid.contentDocument.body.offsetHeight) {
+                iframeid.height = iframeid.contentDocument.body.offsetHeight + 50;
+            } else if (iframeid.Document && iframeid.Document.body.scrollHeight) {
+                frameid.height = iframeid.Document.body.scrollHeight + 50;
+            }
+        }
+    }
+}
+
+function iFrameHeight(id) {
+// 	console.log(document.getElementById(id).contentWindow);
+    var ifm= document.getElementById(id);
+    var subWeb = document.frames ? document.frames[id].document : ifm.contentDocument;
+    console.log("subWeb:" + document.frames[id].document + "/" + ifm.contentDocument);
+    if(ifm != null && subWeb != null) {
+    	ifm.height = subWeb.body.scrollHeight;
+    	console.log("iFrameHeight:", ifm.height);
+    }
+}
