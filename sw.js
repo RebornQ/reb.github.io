531c03269f6f4091017da829f9bf688106413c10
Reborn <ren_xiaoyao@outlook.com>
2020-08-18 20:33:00 +0000

    rebuilding site Tue Aug 18 20:32:58 UTC 2020

---
+++ sw.js
@@ -0,0 +1,117 @@
+importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn/workbox/workbox-sw.js');
+
+if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+
+    workbox.core.setCacheNameDetails({
+        prefix: "Reborn"
+    });
+
+    workbox.core.skipWaiting();
+    workbox.core.clientsClaim();
+
+    workbox.precaching.precacheAndRoute([
+        {
+            "url": "/index.html",
+            "revision": "20e5f6f1230939490b94bb1653da9912"
+        },
+        {
+            "url": "/js/theme.min.js",
+            "revision": "7bc7989e979c4a19d97db3ab311a80fe"
+        },
+        {
+            "url": "/css/style.min.css",
+            "revision": "f03a2ec71b16efbe4f24352e542c93a9"
+        }
+    ], {});
+
+    workbox.routing.registerRoute(/(?:\/)$/,
+        new workbox.strategies.StaleWhileRevalidate({
+            cacheName: "html",
+            plugins: [
+                new workbox.expiration.ExpirationPlugin({
+                    maxAgeSeconds: 60 * 60 * 24 * 7,
+                    // purgeOnQuotaError: !0
+                })
+            ]
+        }), "GET");
+
+    workbox.routing.registerRoute(
+        /\.(?:js|css)$/,
+        new workbox.strategies.StaleWhileRevalidate({
+            cacheName: 'static-resources'
+        })
+    )
+
+    workbox.routing.registerRoute(
+        /\.(?:png|jpg|jpeg|gif|bmp|webp|svg|ico)$/,
+        new workbox.strategies.CacheFirst({
+            cacheName: "images",
+            plugins: [new workbox.expiration.ExpirationPlugin({
+                maxEntries: 100,
+                maxAgeSeconds: 7 * 24 * 60 * 60,
+                // purgeOnQuotaError: !0
+            })]
+        }), "GET");
+
+    // Fonts
+    workbox.routing.registerRoute(
+        /\.(?:eot|ttf|woff|woff2)$/,
+        new workbox.strategies.CacheFirst({
+            cacheName: "fonts",
+            plugins: [
+                new workbox.expiration.ExpirationPlugin({
+                    maxEntries: 1000,
+                    maxAgeSeconds: 60 * 60 * 24 * 30
+                }),
+                new workbox.cacheableResponse.CacheableResponsePlugin({
+                    statuses: [0, 200]
+                })
+            ]
+        })
+    );
+
+    workbox.routing.registerRoute(
+        /^https:\/\/fonts\.googleapis\.com/,
+        new workbox.strategies.StaleWhileRevalidate({
+            cacheName: 'google-fonts-stylesheets'
+        })
+    );
+
+    workbox.routing.registerRoute(
+        /^https:\/\/fonts\.gstatic\.com/,
+        new workbox.strategies.CacheFirst({
+            cacheName: 'google-fonts-webfonts',
+            plugins: [
+                new workbox.cacheableResponse.CacheableResponsePlugin({
+                    statuses: [0, 200]
+                }),
+                new workbox.expiration.ExpirationPlugin({
+                    maxAgeSeconds: 60 * 60 * 24 * 365,
+                    maxEntries: 30
+                })
+            ]
+        })
+    );
+
+    // external resources
+    workbox.routing.registerRoute(
+        /(^https:\/\/cdn\.jsdelivr\.net)|(^https:\/\/cdnjs\.cloudflare\.com)/,
+        new workbox.strategies.CacheFirst({
+            cacheName: "external-resources",
+            plugins: [
+                new workbox.expiration.ExpirationPlugin({
+                    maxEntries: 1000,
+                    maxAgeSeconds: 60 * 60 * 24 * 30
+                }),
+                new workbox.cacheableResponse.CacheableResponsePlugin({
+                    statuses: [0, 200]
+                })
+            ]
+        })
+    );
+
+    workbox.googleAnalytics.initialize({});
+} else {
+    console.log(`Boo! Workbox didn't load 😬`)
+}
