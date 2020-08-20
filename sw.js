aa484f8572851015132556113d8fb360690aa14f
Reborn <ren_xiaoyao@outlook.com>
2020-08-20 20:57:21 +0000

    rebuilding site Thu Aug 20 20:57:19 UTC 2020

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ if (workbox) {
 
     workbox.core.setCacheNameDetails({
         prefix: "Reborn",
-        suffix: 'v6',
+        suffix: 'v5',
         precache: 'precache',
         runtime: 'runtime'
     });
@@ -117,6 +117,13 @@ if (workbox) {
             ]
         })
     );*/
+
+    workbox.googleAnalytics.initialize({});
+
+    // 安装阶段跳过等待，直接进入 active
+    self.addEventListener('install', function (event) {
+        event.waitUntil(self.skipWaiting());
+    });
 
     // Call Activate Event to remove old cache
     self.addEventListener('activate', function (event) {
