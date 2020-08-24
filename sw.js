288f4685af9075674a4c212e09ca07e4d45af047
Reborn <ren_xiaoyao@outlook.com>
2020-08-24 17:35:11 +0000

    rebuilding site Mon Aug 24 17:35:10 UTC 2020

--- sw.js
+++ sw.js
@@ -3,14 +3,19 @@ importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn/workbox/workbox-sw.js');
 if (workbox) {
     console.log(`Yay! Workbox is loaded 🎉`);
 
-    workbox.setConfig({ debug: false });
+    workbox.setConfig({debug: false});
+
+    version = "v5";
+    versionTag = ".0";
+    versionName = version + versionTag;
 
     workbox.core.setCacheNameDetails({
         prefix: "Reborn",
-        suffix: 'v5',
+        suffix: versionName,
         precache: 'precache',
         runtime: 'runtime'
     });
+
 
     // 跳过等待期
     workbox.core.skipWaiting();
@@ -27,12 +32,16 @@ if (workbox) {
         {
             "url": "https://cdn.jsdelivr.net/gh/RebornQ/rebornQ.github.io/css/style.min.css",
             "revision": "7874eca090efe385534dd977d279fa7b"
+        },
+        {
+            "url": "/svg/loading.min.svg",
+            "revision": "4055d59e164001e9375d89ed9ddc38f2"
         }
     ], {});
 
     workbox.routing.registerRoute(/(?:\/)$/,
         new workbox.strategies.NetworkFirst({
-            cacheName: "html-" + workbox.core.cacheNames.suffix,
+            cacheName: "html-" + versionName,
             plugins: [
                 new workbox.expiration.ExpirationPlugin({
                     maxAgeSeconds: 60 * 60 * 24 * 2,
@@ -44,14 +53,14 @@ if (workbox) {
     workbox.routing.registerRoute(
         /\.(?:js|css)$/,
         new workbox.strategies.StaleWhileRevalidate({
-            cacheName: 'static-resources-' + workbox.core.cacheNames.suffix
+            cacheName: 'static-resources-' + versionName
         })
     );
 
     workbox.routing.registerRoute(
         /\.(?:png|jpg|jpeg|gif|bmp|webp|svg|ico)$/,
         new workbox.strategies.CacheFirst({
-            cacheName: "images-" + workbox.core.cacheNames.suffix,
+            cacheName: "images-" + versionName,
             plugins: [
                 new workbox.expiration.ExpirationPlugin({
                     maxEntries: 100,
@@ -65,7 +74,7 @@ if (workbox) {
     workbox.routing.registerRoute(
         /\.(?:eot|ttf|woff|woff2)$/,
         new workbox.strategies.CacheFirst({
-            cacheName: "fonts-" + workbox.core.cacheNames.suffix,
+            cacheName: "fonts-" + versionName,
             plugins: [
                 new workbox.expiration.ExpirationPlugin({
                     maxEntries: 1000,
@@ -81,14 +90,14 @@ if (workbox) {
     workbox.routing.registerRoute(
         /^https:\/\/fonts\.googleapis\.com/,
         new workbox.strategies.StaleWhileRevalidate({
-            cacheName: 'google-fonts-stylesheets-' + workbox.core.cacheNames.suffix
+            cacheName: 'google-fonts-stylesheets-' + versionName
         })
     );
 
     workbox.routing.registerRoute(
         /^https:\/\/fonts\.gstatic\.com/,
         new workbox.strategies.CacheFirst({
-            cacheName: 'google-fonts-webfonts-' + workbox.core.cacheNames.suffix,
+            cacheName: 'google-fonts-webfonts-' + versionName,
             plugins: [
                 new workbox.cacheableResponse.CacheableResponsePlugin({
                     statuses: [0, 200]
@@ -105,7 +114,7 @@ if (workbox) {
     /*workbox.routing.registerRoute(
         /(^https:\/\/cdn\.jsdelivr\.net.*?(\.js|\.css))|(^https:\/\/cdnjs\.cloudflare\.com)/,
         new workbox.strategies.CacheFirst({
-            cacheName: "external-resources-" + workbox.core.cacheNames.suffix,
+            cacheName: "external-resources-" + versionName,
             plugins: [
                 new workbox.expiration.ExpirationPlugin({
                     maxEntries: 1000,
@@ -123,22 +132,19 @@ if (workbox) {
     // Call Activate Event to remove old cache
     self.addEventListener('activate', function (event) {
         event.waitUntil(
-            Promise.all([
-                // 更新客户端
-                self.clients.claim(),
-
-                // 清理旧版本
-                caches.keys().then(function (cacheList) {
-                    return Promise.all(
-                        cacheList.map(function (cacheName) {
-                            if (/(v\d+)/.test(cacheName) === false || workbox.core.cacheNames.suffix !== RegExp.$1) {
-                                return caches.delete(cacheName);
-                            }
-                        })
-                    );
-                })
-            ])
+            caches.keys().then(function (cacheList) {
+                return Promise.all(
+                    cacheList.map(function (cacheName) {
+                        if (/(v[\d+\.]+)/.test(cacheName) === false || versionName !== RegExp.$1) {
+                            // When it doesn't match any condition, delete it.
+                            console.info('version changed, clean the cache, SW: deleting ' + cacheName);
+                            return caches.delete(cacheName);
+                        }
+                    })
+                );
+            })
         );
+        return self.clients.claim();
     });
 } else {
     console.log(`Boo! Workbox didn't load 😬`)
